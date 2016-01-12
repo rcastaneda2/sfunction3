b0467f89b77a3ffa50e2385ef74bffb6d3c591fd
Ruben Castaneda <z5rcastaneda@gmail.com>
2016-01-12 09:54:18 -0800

    few changes

--- R/linmod.R
+++ R/linmod.R
@@ -1,2 +1,11 @@
+#' A linear regression function
+#'
+#' Does stuff with data
+#' @param Does linear regression
+#' @keywords line
+#' @export
+#' @examples
+#' linmod(x,y)
+
 linmod <-
 function(x,...)UseMethod("linmod")
